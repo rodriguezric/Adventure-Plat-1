5fa2f25ef136aaeb429f85e3d90fd435943426e6
= <=>
2021-04-21 05:45:18 -0400

    Small update to collision code.

--- scripts/Sign.gd
+++ scripts/Sign.gd
@@ -6,6 +6,6 @@ signal send_message
 export(Array, String) var dialogue
 
 
-func _on_Sign_body_entered(body: Node) -> void:
+func _on_Sign_body_entered(body: Player) -> void:
 	if body is Player:
 		emit_signal("send_message", dialogue)
